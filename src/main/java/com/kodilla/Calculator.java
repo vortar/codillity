6c350f94a7238835b88e57a34536911e6ac09a3c
vortar <dymeq74@gmail.com>
2018-10-01 12:43:08 +0200

    second commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,34 @@
 package com.kodilla;
 
 public class Calculator {
+    public int liczbaA;
+    public int  liczbaB;
+
+    public Calculator (int liczbaA, int liczbaB){
+        this.liczbaA=liczbaA;
+        this.liczbaB=liczbaB;
+
+    }
+
+    public int  dodawanie(){
+        return liczbaB  + liczbaA;
+
+    }
+    public int odejmowanie(){
+        return liczbaA - liczbaB;
+
+    }
+    public int getLiczbaA() {
+        return liczbaA;
+    }
+
+    public int getLiczbaB() {
+        return liczbaB;
+    }
+    public static void main (String args[]){
+        Calculator test = new Calculator(3,3);
+        System.out.println(test.dodawanie());
+
+
+    }
 }
